05d05454b8f25ca5bd8313fa3245b53423f331fd
Магомадов Магомед <magomadov@MacBook-Air-Magomadov.local>
2022-08-25 20:59:46 +0300

    Исправил баги

--- src/Components/MainContent/MainContent.jsx
+++ src/Components/MainContent/MainContent.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import MainWelcome from './MainWelcome/MainWelcome'
 import '../../scss/main/mainContent.scss'
+import MainRooms from './MainRooms/MainRooms'
+import MainSlice from './MainSlice/MainSlice'
 
 
 const MainContent = () => {
   return (
     <main className='main'>
       <MainWelcome />
+      <MainRooms />
+      <MainSlice />
     </main>
   )
 }
