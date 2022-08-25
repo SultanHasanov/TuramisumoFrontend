c09a0e99c9ea913ea42e7ce4b4dde28ad7c00af0
Магомадов Магомед <magomadov@MacBook-Air-Magomadov.local>
2022-08-25 20:31:44 +0300

    сделал main страницу

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Footer from './Components/Footer/Footer'
 import Header from './Components/Header/Header'
 import MainContent from './Components/MainContent/MainContent'
 import MainRooms from './Components/MainContent/MainRooms/MainRooms'
+import MainSlice from './Components/MainContent/MainSlice/MainSlice'
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <div className='App'>
       <MainContent />
       <MainRooms />
+      <MainSlice/>
       <Footer />
     </div>
   )
--- src/Components/MainContent/MainRooms/MainRooms.jsx
+++ src/Components/MainContent/MainRooms/MainRooms.jsx
@@ -2,21 +2,82 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getRooms } from '../../../features/roomsTypes'
 import '../../../scss/main/mainRooms.scss'
+import Header from '../../Header/Header'
 
 const MainRooms = () => {
-    const dispatch = useDispatch()
-    // const rooms = useSelector((state) => state.room)
-    // console.log()
+  const dispatch = useDispatch()
 
+  useEffect(() => {
+    dispatch(getRooms())
+  }, [dispatch])
 
-    // useEffect(() => {
-    //     dispatch(getRooms())
-    // }, [dispatch])
+  const loadingRooms = useSelector(state => state.room.loadingRooms)
+  const rooms = useSelector(state => state.room.rooms)
 
-  return (
-    <div>
-    </div>
-  )
+  if (loadingRooms) {
+    return <div>загрузка</div>
+  } else {
+    const business = rooms.filter(item => {
+      return (
+        item.name !== 'Номер для молодожен' &&
+        item.name !== 'Семейный номер' &&
+        item.name !== 'Одиночный номер' &&
+        item.name !== 'Парный номер'
+      )
+    })
+
+    const remainder = rooms.filter(item => {
+      return (
+        item.name !== 'Президентский номер' && item.name !== 'Люксовый номер'
+      )
+    })
+
+    const upper = business.map(item => {
+      return (
+        
+          <div className='business_rooms_el'>
+            <div className='business_image'>
+              <img src={item.images[0]} alt='err' />
+            </div>
+            <div className='info_pole'>
+              <div className='name_rooms'>{item.name}</div>
+              <div className='todo_text'>Гостиная</div>
+              <div className='info_pole_footer'>
+                <button className='booking_button'>ЗАБРОНИРОВАТЬ СЕЙЧАС</button>
+                <div className='price_info'>299 ₽</div>
+              </div>
+            </div>
+          </div>
+        
+      )
+    })
+    const bottom = remainder.map((item) => {
+      return (
+        <div className='general_rooms'>
+          <div className='general_image'>
+            <img src={item.images[0]} alt='err' />
+          </div>
+          <div className='info_pole_general'>
+            <div className='name_rooms_general'>{item.name}</div>
+            <div className='todo_text_general'>Гостиная</div>
+            <div className='info_pole_footer_general'>
+              <button className='booking_button_general'>ЗАБРОНИРОВАТЬ СЕЙЧАС</button>
+              <div className='price_info_general'>299 ₽</div>
+            </div>
+          </div>
+        </div>
+      )
+    })
+    return (
+      <div className='container'>
+        <div className='business_rooms'>{upper}</div>
+        <div className='general'>
+         {bottom}
+        </div>
+       
+      </div>
+    )
+  }
 }
 
 export default MainRooms
