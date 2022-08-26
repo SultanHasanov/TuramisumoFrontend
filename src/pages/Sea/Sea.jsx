50e719758604b22e705efe81fc0c02a8a7beaff4
Ross <rabik95@bk.ru>
2022-08-26 14:31:05 +0300

    Добавил sea

---
+++ src/pages/Sea/Sea.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from "../../Components/Header/Header";
+import styles from "../../scss/pages/Relax.module.scss";
+
+export default function Sea() {
+  return (
+    <div>
+        <Header />
+        ADS
+    </div>
+  )
+}
